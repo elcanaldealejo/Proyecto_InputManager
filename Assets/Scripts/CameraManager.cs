ab7ea51407053f242733474650ba37ec47f1ffd0
elcanaldealejo <alejova1991@gmail.com>
2022-02-25 19:05:51 -0500

    Initial commit

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minY_estrellas;
+    public float minY;
+    public Transform SeguirRocket1;
+    public Transform SeguirRocket2;
+    public Transform Estrellas;
+   
+    void Start()
+    {
+        this.transform.position = new Vector3(this.transform.position.x , minY , this.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       if(SeguirRocket1.position.y > minY){
+            if(this.transform.position.y>=minY){
+                this.transform.position = new Vector3(this.transform.position.x , SeguirRocket1.position.y  , this.transform.position.z);
+
+                if( SeguirRocket1.position.y >= Estrellas.position.y  )
+                    Estrellas.position = new Vector2(this.transform.position.x , SeguirRocket1.position.y );
+                    
+                if(SeguirRocket1.position.y < Estrellas.position.y && Estrellas.position.y>minY_estrellas)
+                    Estrellas.position = new Vector2(this.transform.position.x , SeguirRocket1.position.y );
+            }       
+        }    
+        if(SeguirRocket2.position.y > minY){
+            if(this.transform.position.y>=minY){
+                this.transform.position = new Vector3(this.transform.position.x , SeguirRocket2.position.y  , this.transform.position.z);
+
+                if( SeguirRocket2.position.y >= Estrellas.position.y  )
+                    Estrellas.position = new Vector2(this.transform.position.x , SeguirRocket2.position.y );
+                    
+                if(SeguirRocket2.position.y < Estrellas.position.y && Estrellas.position.y>minY_estrellas)
+                    Estrellas.position = new Vector2(this.transform.position.x , SeguirRocket2.position.y );
+            }       
+        }   
+        
+    }
+}
---
+++ Assets/Scripts/Volar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Luminosity.IO;
+
+
+public class Volar : MonoBehaviour
+{
+    public float FuerzaVuelo;
+    public bool Mobile;
+        
+    private Rigidbody2D Rb2D;
+    private Animator _animator;
+    private Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rb2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+   
+    void FixedUpdate() {
+        
+        velocity = new Vector2(0f ,1f*FuerzaVuelo);
+        if(!Mobile && InputManager.PlayerOneControlScheme.Name == "ControlPC"){
+            //Interacción desde Teclado
+            if( InputManager.GetAxisRaw("Action") > 0f  ){
+                _animator.SetBool("Ignition", true);
+                Rb2D.MovePosition( Rb2D.position + velocity * Time.fixedDeltaTime);
+            }else{
+                _animator.SetBool("Ignition", false);
+            }
+        }
+        if(Mobile && InputManager.PlayerOneControlScheme.Name == "ControlMobile"){
+            //Interacción desde Teclado
+            if( InputManager.GetButton("Action")  ){
+                _animator.SetBool("Ignition", true);
+                Rb2D.MovePosition( Rb2D.position + velocity * Time.fixedDeltaTime);
+            }else{
+                _animator.SetBool("Ignition", false);
+            }
+        }
+    }
+
+    
+}
